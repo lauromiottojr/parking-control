fd5b8299e6499cb68a34414479b0ca185810ac15
lauromiottojr <junior.miotto36@gmail.com>
2022-11-21 15:46:48 -0300

    ManyToMany RoleModel

--- src/main/java/com/api/parkingcontrol/models/RoleModel.java
+++ src/main/java/com/api/parkingcontrol/models/RoleModel.java
@@ -2,6 +2,7 @@ package com.api.parkingcontrol.models;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 import javax.persistence.Column;
@@ -11,6 +12,9 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 import org.springframework.security.core.GrantedAuthority;
@@ -30,6 +34,10 @@ public class RoleModel implements UserDetails, Serializable, GrantedAuthority {
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false, unique = true)
 	private RoleName roleName;
+
+	@ManyToMany
+	@JoinTable(name = "TB_USERS_ROLES", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private List<RoleModel> roles;
 
 	public UUID getId() {
 		return id;
