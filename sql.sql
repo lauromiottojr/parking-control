4f474a9ce5afc0faa1d946940ef2facd06585871
lauromiottojr <junior.miotto36@gmail.com>
2022-11-22 13:16:36 -0300

    criando classe UserModel e UserDetailsServiceImpl

--- sql.sql
+++ sql.sql
@@ -1,10 +1,15 @@
+drop database parking_control;
 truncate table tb_user;
 truncate table tb_users_roles;
+truncate table tb_role;
+truncate table tb_parking_spot;
 use parking_control;
 
-insert into tb_user values (1, '$2a$10$szdEDiwa8snlxsD2MJKOr.Ez.74vtXm7rpGnGI66wKntBgCT7E7y2', 'lauro');
+create database parking_control;
 
-insert into tb_user values (2, '$2a$10$szdEDiwa8snlxsD2MJKOr.Ez.74vtXm7rpGnGI66wKntBgCT7E7y2', 'joao');
+insert into tb_user values ('1', '$2a$10$gAyMC2WtFNAXkN.AfhfbI.IU7xAZwF3Lrpw7.1SoXV/pQusYXtCwa', 'admin');
+
+insert into tb_user values ('2', '$2a$10$gAyMC2WtFNAXkN.AfhfbI.IU7xAZwF3Lrpw7.1SoXV/pQusYXtCwa', 'user');
 
 select * from tb_user;
 
---
+++ src/main/java/com/api/parkingcontrol/models/UserModel.java
@@ -0,0 +1,86 @@
+package com.api.parkingcontrol.models;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Entity
+@Table(name = "TB_USER")
+public class UserModel implements Serializable, UserDetails {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+
+	@Column(nullable = false, unique = true)
+	private String username;
+
+	@Column(nullable = false)
+	private String password;
+
+	// GET AND SETTERS -----------------------------------------------------
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	// USER DETAILS METHODS --------------------------------------------
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+}
